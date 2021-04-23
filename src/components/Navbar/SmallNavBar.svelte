29387c15710a9743a641ff188a0317fdfc07771d
Mia <miamorticia@Mias-MacBook-Pro.local>
2021-04-23 17:11:29 -0600

    deploy run

--- src/components/Navbar/SmallNavBar.svelte
+++ src/components/Navbar/SmallNavBar.svelte
@@ -6,20 +6,20 @@
     </script>
 
     <nav class="navbar">
-    <div class="nav-center">
-    <!-- sidebar toggle -->
-    <button class="btn-sidebar-toggle"
-    on:click={() => {
-    openSidebar('sidebar', true);
-    }}>
-    <i class="fas fa-bars" />
+        <div class="nav-center">
+            <!-- sidebar toggle -->
+            <button 
+            class="btn-sidebar-toggle"
+                on:click={() => {
+                openSidebar('sidebar', true);
+            }}>
+        <i class="fas fa-bars" />
     </button>
     <!-- logo -->
     <a href="/" use:link class="nav-logo">
         <img src="/assets/images/logo.png" class="logo" alt="razors logo" />
     </a>
     <!-- cart button -->
-    <!-- <button>cart</button> -->
     <CartButton />
     </div>
 </nav>
